67b56be4f6ecab07631af4a20b97487331eb71e0
Jake Probst <jake.probst@gmail.com>
2019-06-23 15:52:54 -0700

    PSOPacket is Debug, more parse errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
-mod crypto;
-mod patch;
+pub mod crypto;
+pub mod patch;
 
 #[derive(Debug, PartialEq)]
 pub enum PacketParseError {
     NotEnoughBytes,
     WrongPacketCommand,
+    WrongPacketForServerType,
+    WrongPacketSize(u16, usize),
+    DataStructNotLargeEnough(u64, usize),
 }
 
 
-pub trait PSOPacket {
+pub trait PSOPacket: std::fmt::Debug {
     fn from_bytes(data: &Vec<u8>) -> Result<Self, PacketParseError> where Self: Sized;
     fn as_bytes(&self) -> Vec<u8>;
 }
